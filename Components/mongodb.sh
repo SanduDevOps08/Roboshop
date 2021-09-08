6c44c96eeee2f1b19b3cc8c7bd5be6306547c932
Cloud User <centos@ip-172-31-89-36.ec2.internal>
2021-09-08 14:54:08 +0000

    Updating the mongodb script with functional indentation

--- Components/mongodb.sh
+++ Components/mongodb.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-echo "Creating the repository"
+Status_Check()
+{
+if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILURE\e[0m"
+exit 2
+fi
+}
+
+Print()
+{
+echo -n -e "$1 \t-"
+}
+
+Print "Creating the repository\t\t"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -8,36 +23,46 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+Status_Check $?
 
-echo "Installing and starting the service"
+Print "Installing and starting the service"
+yum install -y mongodb-org &>>/tmp/log 
+Status_Check $?
 
-yum install -y mongodb-org 
 
-echo "Configuring the Mongodb"
+Print "Configuring the Mongodb\t\t"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/log
+Status_Check $?
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-echo "starting mongodb"
-
+Print "starting mongodb\t\t"
 systemctl enable mongod
-systemctl start mongod
+systemctl start mongod &>>/tmp/log
+Status_Check $?
 
-echo "Downloading schema"
 
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+Print "Downloading schema\t\t"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip\t\t" &>>/tmp/log
+Status_Check $?
+
 cd /tmp
 
-echo "Extracting the zip files"
-unzip mongodb.zip
+Print "Extracting the zip files\t"
+unzip -o mongodb.zip &>>/tmp/log
+Status_Check $?
+
+
 cd mongodb-main
 
-echo "Loading the schema"
+Print "Loading the schema\t\t"
+mongo < catalogue.js &>>/tmp/log
+mongo < users.js &>>/tmp/log
+Status_Check $?
 
-mongo < catalogue.js
-mongo < users.js 
 
-echo "Restarting mongodb"
+Print "Restarting mongodb\t\t"
+systemctl start mongod &>>/tmp/log
+Status_Check $?
 
-systemctl start mongod
 
-echo "Execution completed"
+exit 0
