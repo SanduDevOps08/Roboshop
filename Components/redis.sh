e0127a901b84a3c352acf6a1362b3e5ec3f5eb77
SanduDevOps08 <sandubejai@gmail.com>
2021-09-13 04:18:40 +0000

    Adding redis script

---
+++ Components/redis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source Components/common.sh
+
+Print "Installing yum utils and downloading Redis repos\t\t\t"
+
+yum install epel-release yum-utils yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+Status_Check $?
+
+Print "Setup Redis repos\t\t\t"
+
+yum-config-manager --enable remi &>>$LOG
+Status_Check $?
+
+Print "Install Redis repos\t\t\t\t"
+
+yum install redis -y &>>$LOG
+Status_Check $?
+
+Print "Updating Redis IP address\t\t\t"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>$LOG
+Status_Check $?
+
+Print "Start Redis service\t\t"
+
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
+Status_Check $?
