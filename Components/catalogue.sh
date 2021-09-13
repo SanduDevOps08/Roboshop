c90348cf69cf92a3c74d714961d4d41ab72f6fd8
SanduDevOps08 <sandubejai@gmail.com>
2021-09-13 03:28:42 +0000

    Updating changes to the catalogue file

--- Components/catalogue.sh
+++ Components/catalogue.sh
@@ -7,9 +7,14 @@ Print "Installing nodejs packages\t\t"
 yum install nodejs make gcc-c++ -y &>>$LOG
 Status_Check $?
 
-Print "Adding the Roboshop suser"
+Print "Adding the Roboshop user"
 
+id Roboshop &>>$LOG
+if [ $? -eq 0]; then
+ echo "The user already exists, hence skipping" &>>$LOG
+else 
 useradd -G wheel roboshop &>>$LOG
+fi
 Status_Check $?
 
 Print "Downloading Catalogue zipfile content"
@@ -17,34 +22,26 @@ Print "Downloading Catalogue zipfile content"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
 Status_Check $?
 
-cd /home/roboshop
-
 Print "Extracting the Catalogue files"
 
-unzip -o /tmp/catalogue.zip &>>$LOG
-mv catalogue-main catalogue
+cd /home/roboshop
+rm -rf catalogue && unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue
 Status_Check $?
 
-Print "Changing to catalogue directory"
+Print "Download nodeJS dependencies"
 
 cd /home/roboshop/catalogue &>>$LOG
+npm install --unsafe-perm &>>$LOG
 Status_Check $?
 
-npm install 
+Print "Update systemD service"
+
+sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/Roboshop/catalogue/systemd.service
 Status_Check $?
 
-Print "Moving the file from system.service to Catologue.service"
+Print "Setup systemD service"
 
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG
-Status_Check $?
-
-systemctl daemon-reload &>>$LOG
-Status_Check $?
-
-systemctl start catalogue &>>$LOG
-Status_Check $?
-
-systemctl enable catalogue
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG && systemctl daemon-reload && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
 Status_Check $?
 
 exit 0
